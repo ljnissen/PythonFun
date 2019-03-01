897ed7e830f09b2c8be0f8318d236d9676512c16
ljnissen <lj_nissen@gmx.de>
2019-03-01 19:44:44 +0000

    Add rectangle

--- MathFunctions.py
+++ MathFunctions.py
@@ -9,3 +9,19 @@ def areaCircle(radius):
 def areaSquare(side):
 	area = side * side
 	return area
+
+def areaRectangle(length, width):
+	area = length * width
+	return area
+
+def x_numbers(number1, number2, number3):
+	x = (number1, number2, number3)
+	return x
+
+def y_numbers(number1, number2, number3):
+	y = (number1, number2, number3)
+	return y
+
+	from pylab import plot, show
+	plot(x_numbers, y_numbers)
+
--- TestMathFunctions.py
+++ TestMathFunctions.py
@@ -36,6 +36,34 @@ class KnownValues(unittest.TestCase):
 		expected = 100
 		self.assertEqual(expected, result)
 
+	def test_areaSquare_for_50side(self):
+		# Capture the results of the function
+		result = MathFunctions.areaSquare(50)
+		# Check for expected output
+		expected = 2500
+		self.assertEqual(expected, result)
+
+	def test_areaRectangle_for_10_50_sides(self):
+		# Capture the results of the function
+		result = MathFunctions.areaRectangle(10, 50)
+		# Check for expected output
+		expected = 500
+		self.assertEqual(expected, result)
+
+	def test_x_numbers_for_1_2_3(self):
+		# Capture the results of the function
+		result = MathFunctions.x_numbers(1, 2, 3)
+		# Check for expected output
+		expected = (1, 2, 3)
+		self.assertEqual(expected, result)
+
+	def test_y_numbers_for_2_4_6(self):
+		# Capture the results of the function
+		result = MathFunctions.y_numbers(2, 4, 6)
+		# Check for expected output
+		expected = (2, 4, 6)
+		self.assertEqual(expected, result)
+
 # Run the Tests
 
 if __name__ == '__main__':
